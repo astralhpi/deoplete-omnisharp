22553d8d974bb45ca18e3f1846616d2d5184132d
Song Jaehak <hpi@moonrabbit.co>
2016-02-22 21:02:21 +0900

    initial commit

---
+++ rplugin/python3/deoplete/sources/cs.py
@@ -0,0 +1,20 @@
+import deoplete.util
+
+from .base import Base
+
+class Source(Base):
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+
+        self.name = 'cs'
+        self.mark = '[CS]'
+        self.filetypes = ['cs']
+        self.input_pattern = '\.'
+        self.is_bytepos = True
+
+    def get_complete_position(self, context):
+        return self.vim.call('OmniSharp#Complete', 1, 0)
+
+    def gather_candidates(self, context):
+        return self.vim.call('OmniSharp#Complete', 0, 0)
+
